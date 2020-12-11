46b17089848c0bcc4b161c014646a7f400e7307b
Nithin Gunasekaran <nithin.ktg@gmail.com>
2020-12-11 19:28:49 +0530

    Fix build

--- db/migrate/20201211124123_add_processed_bank_transaction_to_double_entry_transaction.rb
+++ db/migrate/20201211124123_add_processed_bank_transaction_to_double_entry_transaction.rb
@@ -1,6 +1,5 @@
 class AddProcessedBankTransactionToDoubleEntryTransaction < ActiveRecord::Migration[6.0]
   def change
     add_column :double_entry_transactions, :processed_bank_transaction_id, :integer
-    remove_column :processed_bank_transactions, :double_entry_transaction_id, :integer
   end
 end

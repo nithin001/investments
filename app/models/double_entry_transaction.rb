750abf3bcd679fe441691c45edeacd54d5a0a087
Nithin Gunasekaran <nithin.ktg@gmail.com>
2021-06-30 13:21:19 +0530

    Remove unneccessary files

--- app/models/double_entry_transaction.rb
+++
@@ -1,7 +0,0 @@
-class DoubleEntryTransaction < ApplicationRecord
-  belongs_to :member
-  belongs_to :credit_account, class_name: 'Account', foreign_key: :credit_account_id
-  belongs_to :debit_account, class_name: 'Account', foreign_key: :debit_account_id
-  belongs_to :processed_bank_transaction
-  has_and_belongs_to_many :tags
-end
